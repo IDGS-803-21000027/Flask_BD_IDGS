26b695f47e39e3070a7881202d9b1b8388bc5ba7
IDGS-803-21000027 <21000027@alumnos.utleon.edu.mx>
2024-03-01 18:10:30 -0600

    Eliminar y modificar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from flask_wtf.csrf import CSRFProtect
-from flask import g
+from flask import g, redirect
 
 from config import DevelopmentConfig
 from flask import flash
@@ -28,7 +28,46 @@ def index():
         db.session.commit()
     return render_template("index.html", form=alum_form)
 
-@app.route("/ABC_Completo")
+@app.route("/eliminar",methods=["GET","POST"])
+def eliminar():
+    alum_form=forms.UsersForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum_form.id.data=request.args.get('id')
+        alum_form.nombre.data=alum1.nombre
+        alum_form.apaterno.data=alum1.apaterno
+        alum_form.email.data=alum1.email
+    if request.method=='POST':
+        id=alum_form.id.data
+        alum=Alumnos.query.get(id)
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect('ABC_Completo')
+    return render_template("eliminar.html", form=alum_form)
+
+@app.route("/modificar",methods=["GET","POST"])
+def modificar():
+    alum_form=forms.UsersForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum_form.id.data=request.args.get('id')
+        alum_form.nombre.data=alum1.nombre
+        alum_form.apaterno.data=alum1.apaterno
+        alum_form.email.data=alum1.email
+    if request.method=='POST':
+        id=alum_form.id.data
+        alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum1.nombre = alum_form.nombre.data
+        alum1.apaterno = alum_form.apaterno.data
+        alum1.email = alum_form.email.data
+        db.session.add(alum1)
+        db.session.commit()
+        return redirect('ABC_Completo')
+    return render_template("modificar.html", form=alum_form)
+
+@app.route("/ABC_Completo",methods=["GET","POST"])
 def ABC_Completo():
     alum_form=forms.UsersForm2(request.form)
     alumno=Alumnos.query.all()
